57e3045c32829533822d6b4c67d79a819f743604
Kaspar von Gunten <kvgunten@gmx.net>
2020-03-08 14:20:47 +0100

    Add dropin-impl (Quotes-Dropin)

--- dropin-impl/src/main/java/org/zaploink/example/dropin/impl/QuotesImpl.java
+++ dropin-impl/src/main/java/org/zaploink/example/dropin/impl/QuotesImpl.java
@@ -1,7 +1,19 @@
 package org.zaploink.example.dropin.impl;
 
-public interface QuotesImpl implements Quotes {
-    String getQuoteOfTheDay() {
+import org.zaploink.example.dropin.def.Quotes;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+public class QuotesImpl implements Quotes {
+    private String[] quotes = {
+      "An apple a day keeps the doctor away. -- Benjamin Franklin",
+      "In the middle of every difficulty lies opportunity. -- Albert Einstein",
+      "Blessed is the man who expects nothing, for he shall never be disappointed. -- Alexander Pope",
+      "From little acorns mighty oaks do grow. -- American proverb",
+    };
+
+    @Override
+    public String getQuoteOfTheDay() {
+        return quotes[ThreadLocalRandom.current().nextInt(quotes.length)];
     }
 }
